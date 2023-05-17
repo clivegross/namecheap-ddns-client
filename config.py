ac011e562b35da9f9cf435a6ba9ce851004ebb29
Clive <clive.gross1@gmail.com>
2023-05-17 18:12:44 +1000

    config.py improvements
    Handle config values of int data type

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import configparser
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-def read(value):
-    return config['DEFAULT'][value]
+def read(value, type=str):
+    if type==str:
+        return config['DEFAULT'][value]
+    elif type==int:
+        return config['DEFAULT'].getint(value)
+    else:
+        return config['DEFAULT'][value]
